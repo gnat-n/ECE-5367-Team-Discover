c095448ff930ed6ed593fc990c03c7b3b5b8859e
MinionOP <73415249+MinionOP@users.noreply.github.com>
2021-04-21 01:04:21 -0500

    Update AES_encrypt.cpp

--- source/AES_encrypt.cpp
+++ source/AES_encrypt.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 #include "tables.h"
+#include <sstream>
+#include <bitset>
+
+using namespace::std;
 
 void printCheck(unsigned char state[][4]);
-void KeyExpansion();
+void KeyExpansion(unsigned char keySchedule[44][4], int round);
 void SubBytes(unsigned char state[][4]);
 void ShiftRows(unsigned char state[][4]);
 void MixColumns(unsigned char state[][4]);
-void AddRoundKeys(unsigned char state[][4], char* key);
-void encrypt(unsigned char* input, char* key);
+void AddRoundKeys(unsigned char state[][4], unsigned char key[][4], int round);
+
+void encrypt(unsigned char input[17], unsigned char key[][4]);
+void decrypt(unsigned char input[17], unsigned char key[][4]);
 
 void InvSubBytes(unsigned char state[][4]);
-void InvSubBytes_helper(unsigned char s_box[][16],unsigned char need, int &r, int &c);
 void InvShiftRows(unsigned char state[][4]);
 void InvMixColumns(unsigned char state[][4]);
 
-int main() {
-	unsigned char temp[16] = "TestingTestin12";
-	char key[16] = "TestingTestin12";
-	encrypt(temp,key);
+int main(){
 
+  unsigned char temp[17] ="Two One Nine Two";
+  
+	unsigned char key[4][4] = {
+		0x54,0x68,0x61,0x74,
+		0x73,0x20,0x6D,0x79,
+		0x20,0x4B,0x75,0x6E,
+		0x67,0x20,0x46,0x75
+	};
+
+  /*unsigned char temp[17] ={0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff};
+
+  unsigned char key[4][4] = {
+    0x00,0x01,0x02,0x03,
+    0x04,0x05,0x06,0x07,
+    0x08,0x09,0x0a,0x0b,
+    0x0c,0x0d,0x0e,0x0f
+  };
+  */
+  /*unsigned char key[4][4] = {
+    0x00,0x04,0x08,0x0c,
+    0x01,0x05,0x09,0x0d,
+    0x02,0x06,0x0a,0x0e,
+    0x03,0x07,0x0b,0x0f
+  };*/
 	/*unsigned char test[4][4] = {
 		0x00,0x3c,0x6e,0x47,
 		0x1f,0x4e,0x22,0x74,
@@ -35,87 +61,207 @@ int main() {
 	//};
 
 	//After shift rows
-	//unsigned char test[4][4] = {
-	//	0x63,0xeb,0x9f,0xa0,
-	//	0x2f,0x93,0x92,0xc0,
-	//	0xaf,0xc7,0xab,0x30,
-	//	0xa2,0x20,0xcb,0x2b
-	//};
+	/*unsigned char test[4][4] = {
+		0x63,0xeb,0x9f,0xa0,
+		0x2f,0x93,0x92,0xc0,
+		0xaf,0xc7,0xab,0x30,
+		0xa2,0x20,0xcb,0x2b
+	};*/
 
 	//After mix columns
-	unsigned char test[4][4] = {
+	/*unsigned char test[4][4] = {
 		0xba,0x84,0xe8,0x1b,
 		0x75,0xa4,0x8d,0x40,
 		0xf4,0x8d,0x06,0x7d,
 		0x7a,0x32,0x0e,0x5d
-	};
-	InvMixColumns(test);
-	printCheck(test);
+	};*/
 
+	encrypt(temp, key);
+  decrypt(temp, key);
 	return 0;
 }
-void encrypt(unsigned char* input, char* key) {
+	
+
+void printKeySchedule(unsigned char schedule[][4]) {
+	for (int i = 0; i < 11; i++) {
+		std::cout << "Round " << i << ": ";
+		for (int n = 0; n < 4; n++) {
+			for (int j = 0; j < 4; j++) {
+				cout << hex << (int)schedule[i * 4 + n][j] << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+void encrypt(unsigned char input[17], unsigned char key[][4]) {
+	// size is 4 + 4 * rounds, rounds is 10, so size is 44x4
+	unsigned char keySchedule[44][4] = { 0 };
 	unsigned char state[4][4];
 	int pos = 0;
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			state[j][i] = input[pos];
+			keySchedule[i][j] = key[i][j];
 			pos++;
 		}
 	}
-	SubBytes(state);
-	ShiftRows(state);
-	/*AddRoundKeys(state, key);
-	for (int i = 0; i < 9; i++) {
+
+	for (int i = 1; i < 11; i++) {
+		KeyExpansion(keySchedule, i);
+	}
+
+  // round 0
+	AddRoundKeys(state, keySchedule, 0);
+
+	for (int i = 1; i < 10; i++) {
 		SubBytes(state);
 		ShiftRows(state);
 		MixColumns(state);
-		AddRoundKeys(state, key);
+    // round 1 - 9
+		AddRoundKeys(state, keySchedule, i);
 	}
 	SubBytes(state);
 	ShiftRows(state);
-	AddRoundKeys(state, key);*/
+  // round a
+	AddRoundKeys(state, keySchedule, 10);
+
+  pos = 0;
+  for(int i=0;i<4;i++){
+    for(int j=0;j<4;j++){
+      input[pos] = state[j][i];
+      pos++;
+    }
+  }
+  std::cout<<"Encrypted Data: ";
+  for(int i=0;i<16;i++){
+    std::cout<<hex<<(int)input[i]<<" ";
+  }
+  cout << endl;
+}
+
+void decrypt(unsigned char input[16], unsigned char key[][4]) {
+	// size is 4 + 4 * rounds, rounds is 10, so size is 44x4
+	unsigned char keySchedule[44][4] = { 0 };
+	unsigned char state[4][4];
+	int pos = 0;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			state[j][i] = input[pos];
+			keySchedule[i][j] = key[i][j];
+			pos++;
+		}
+	}
+
+	for (int i = 1; i < 11; i++) {
+		KeyExpansion(keySchedule, i);
+	}
+
+  // round 0
+	AddRoundKeys(state, keySchedule, 10);
+
+  InvShiftRows(state);
+  std::cout<<"Shift Rows:"<<std::endl;
+  printCheck(state);
+
+  InvSubBytes(state);
+  std::cout<<"Sub Bytes:"<<std::endl;
+  printCheck(state);
+
+	for (int i = 9; i > 0; i--) {
+    // round 1 - 9
+		AddRoundKeys(state, keySchedule, i);
+    
+    InvMixColumns(state);
+    std::cout<<"Mix Columns:"<<std::endl;
+    printCheck(state);
+
+    InvShiftRows(state);
+    std::cout<<"Shift Rows:"<<std::endl;
+    printCheck(state);
+
+    InvSubBytes(state);
+    std::cout<<"Sub Bytes:"<<std::endl;
+    printCheck(state);
+    
+	}
+  // round a
+	AddRoundKeys(state, keySchedule, 0);
+
+  pos = 0;
+  for(int i=0;i<4;i++){
+    for(int j=0;j<4;j++){
+      input[pos] = state[j][i];
+      pos++;
+    }
+  }
+  std::cout<<"Decrypted Data: ";
+  for(int i=0;i<16;i++){
+    std::cout<<hex<<(int)input[i]<<" ";
+  }
+  cout << endl;
 }
 
 
-void KeyExpansion() {
+void KeyExpansion(unsigned char schedule[][4], int round) {
+	// generate the nth key round
+	int row = round * 4;
+	//printRow(schedule, row);
 
-}
+	// part 1: copy previous row into first row of this round
+	for (int i = 0; i < 4; i++) {
+		schedule[row][i] = schedule[row - 1][i];
+	}
 
-void SubBytes(unsigned char state[][4]) {
-	unsigned char sbox[16][16] = {
-	    {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76},
-	    {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0},
-	    {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15},
-	    {0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75},
-	    {0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84},
-	    {0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf},
-	    {0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8},
-	    {0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2},
-	    {0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73},
-	    {0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb},
-	    {0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79},
-	    {0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08},
-	    {0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a},
-	    {0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e},
-	    {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf},
-	    {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16},
-	};
-	int MSBs = 0;
-  	int LSBs = 0;
-	for (int i = 0; i < 4; i++){
-		for (int j = 0; j < 4; j++){
-			MSBs = 0;
-			LSBs = 0;
-			MSBs = (state[i][j] & 0xf0);
-			MSBs >>= 4;
-			LSBs = (state[i][j] & 0x0f);
-			state[i][j] = sbox[MSBs][LSBs];
+	// part 1: circular left shift
+	unsigned char temp;
+	temp = schedule[row][0];
+	for (int i = 0; i < 4 - 1; i++) {
+		schedule[row][i] = schedule[row][i + 1];
+	}
+	schedule[row][3] = temp;
+
+	// part 2: s box substitution
+	for (int i = 0; i < 4; i++) {
+		schedule[row][i] = sbox2[(int)(schedule[row][i] / 16)][(int)(schedule[row][i] % 16)];
+	}
+	// part 3: add round constant
+	int rcon = roundConstants[round - 1]; // rcon is a special round constant computed by a complicated formula, the first 10 values are provided
+
+	schedule[row][0] = schedule[row][0] ^ rcon;
+
+	// part 3: perform a special g() xor with current row and n - 4
+	for (int i = 0; i < 4; i++) {
+		schedule[row][i] = schedule[row][i] ^ schedule[row - 4][i];
+	}
+
+	// part 4: next 3 rows are just n - 1 ^ n - 4
+	for (int i = 1; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			schedule[row + i][j] = schedule[row + i - 1][j] ^ schedule[row + i - 4][j];
 		}
 	}
 }
 
+void SubBytes(unsigned char state[][4]) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			state[i][j] = sbox[state[i][j]];
+		}
+	}
+}
 void ShiftRows(unsigned char state[][4]) {
+	/*	(Index, not values)
+		[0]	[4]	[8]	 [12]
+		[1]	[5]	[9]	 [13]
+		[2]	[6]	[10] [14]
+		[3]	[7]	[11] [15]
+	to
+		[0]	 [4]  [8]  [12]
+		[5]	 [9]  [13] [1]
+		[10] [14] [2]  [6]
+		[15] [3]  [7]  [11]
+	*/
 	unsigned char temp[4][4];
 	//1st row (No change)
 	temp[0][0] = state[0][0];
@@ -148,12 +294,30 @@ void ShiftRows(unsigned char state[][4]) {
 	}
 }
 void MixColumns(unsigned char state[][4]) {
+	/* (Values, not index)
+	[s0] [s4] [s8]	[s12]		[2]	[3]	[1]	[1]
+	[s1] [s5] [s9]	[s13]		[1]	[2]	[3]	[1]
+	[s2] [s6] [s10] [s14]		[1]	[1]	[2]	[3]
+	[s3] [s7] [s11] [s15]		[3]	[1]	[1]	[2]
+
+	[2]	[3]	[1]	[1]		[s0]	[s0']
+	[1]	[2]	[3]	[1]  *	[s1] =	[s1']
+	[1]	[1]	[2]	[3]		[s2]	[s2']
+	[3]	[1]	[1]	[2]		[s3]	[s3']
+
+	Example:
+	[s0]
+	[s1]  * [2][3][1][1] = [s0']
+	[s2]
+	[s3]
+	*/
+
 	unsigned char temp[4][4];
 	for (int i = 0; i < 4; i++) {
 		temp[0][i] = (unsigned char)(m2[state[0][i]] ^ m3[state[1][i]] ^ state[2][i] ^ state[3][i]);
 		temp[1][i] = (unsigned char)(state[0][i] ^ m2[state[1][i]] ^ m3[state[2][i]] ^ state[3][i]);
-		temp[2][i] = (unsigned char)(state[0][i] ^ state[1][i] ^ m2[state[2][i]] ^ m3[state[3][i]]);	
-		temp[3][i] = (unsigned char)(m3[state[0][i]] ^ state[1][i] ^ state[2][i] ^ m2[state[3][i]]);	
+		temp[2][i] = (unsigned char)(state[0][i] ^ state[1][i] ^ m2[state[2][i]] ^ m3[state[3][i]]);
+		temp[3][i] = (unsigned char)(m3[state[0][i]] ^ state[1][i] ^ state[2][i] ^ m2[state[3][i]]);
 
 	}
 
@@ -163,43 +327,32 @@ void MixColumns(unsigned char state[][4]) {
 		}
 	}
 }
-void AddRoundKeys(unsigned char state[][4], char* key) {
 
+void AddRoundKeys(unsigned char state[][4], unsigned char key[][4], int round) {
+  std::cout<<"Round: " << round<<std::endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+      std::cout<<hex << (int) key[i+(round*4)][j]<<" ";
+			state[i][j] ^= (unsigned char)key[j+(round*4)][i];
+		}
+	}
+  cout << endl;
+  cout<<"New key: "<<endl;
+  printCheck(key);
+  cout<<"Current State: "<<endl;
+
+  printCheck(state);
+  std::cout<<std::endl;
 }
 
+
 void InvSubBytes(unsigned char state[][4]) {
-	unsigned char inv_sbox[16][16] = {
-		{0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb},
-		{0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb},
-		{0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e},
-		{0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25},
-		{0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92},
-		{0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84},
-		{0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06},
-		{0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b},
-		{0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73},
-		{0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e},
-		{0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b},
-		{0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4},
-		{0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f},
-		{0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef},
-		{0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61},
-		{0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d} 
-	};
-	int MSBs = 0;
-  	int LSBs = 0;
-	for (int i = 0; i < 4; i++){
-		for (int j = 0; j < 4; j++){
-			MSBs = 0;
-			LSBs = 0;
-			MSBs = (state[i][j] & 0xf0);
-			MSBs >>= 4;
-			LSBs = (state[i][j] & 0x0f);
-			state[i][j] = inv_sbox[MSBs][LSBs];
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			state[i][j] = inv_sbox[state[i][j]];
 		}
 	}
 }
-
 void InvShiftRows(unsigned char state[][4]) {
 	unsigned char temp[4][4];
 	//1st row (No change)
@@ -233,12 +386,30 @@ void InvShiftRows(unsigned char state[][4]) {
 	}
 }
 void InvMixColumns(unsigned char state[][4]) {
+	/* (Values, not index)
+	[s0] [s4] [s8]	[s12]		[0E][0B][0D][09]
+	[s1] [s5] [s9]	[s13]		[09][0E][0B][0D]
+	[s2] [s6] [s10] [s14]		[0D][09][0E][0B]
+	[s3] [s7] [s11] [s15]		[0B][0D][09][0E]
+
+	[14][11][13][9]		[s0]	[s0']
+	[9]	[14][11][13]  *	[s1] =	[s1']
+	[13][9]	[14][11]	[s2]	[s2']
+	[11][13][9]	[14]	[s3]	[s3']
+
+	Example:
+	[s0]
+	[s1]  * [14][11][13][9] = [s0']
+	[s2]
+	[s3]
+	*/
+
 	unsigned char temp[4][4];
 	for (int i = 0; i < 4; i++) {
 		temp[0][i] = (unsigned char)(m14[state[0][i]] ^ m11[state[1][i]] ^ m13[state[2][i]] ^ m9[state[3][i]]);
-		temp[1][i] = (unsigned char)(m9[state[0][i]] ^ m14[state[1][i]] ^ m11[state[2][i]] ^ m13[state[3][i]]);	
-		temp[2][i] = (unsigned char)(m13[state[0][i]] ^ m9[state[1][i]] ^ m14[state[2][i]] ^ m11[state[3][i]]);	
-		temp[3][i] = (unsigned char)(m11[state[0][i]] ^ m13[state[1][i]] ^ m9[state[2][i]] ^ m14[state[3][i]]);	
+		temp[1][i] = (unsigned char)(m9[state[0][i]] ^ m14[state[1][i]] ^ m11[state[2][i]] ^ m13[state[3][i]]);
+		temp[2][i] = (unsigned char)(m13[state[0][i]] ^ m9[state[1][i]] ^ m14[state[2][i]] ^ m11[state[3][i]]);
+		temp[3][i] = (unsigned char)(m11[state[0][i]] ^ m13[state[1][i]] ^ m9[state[2][i]] ^ m14[state[3][i]]);
 	}
 
 	for (int i = 0; i < 4; i++) {
@@ -254,7 +425,7 @@ void InvMixColumns(unsigned char state[][4]) {
 void printCheck(unsigned char state[][4]) {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			std::cout << std::hex<<(int)state[i][j]<<"\t";
+			std::cout << std::hex << (int)state[i][j] << "\t";
 		}
 		std::cout << std::endl;
 	}
